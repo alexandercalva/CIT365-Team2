331ca00e41779ab8ab9e7c6c2ce17c9ecac6b7a6
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2020-02-26 10:05:26 -0500

    Update View

--- Pages/Desks/Create.cshtml.cs
+++ Pages/Desks/Create.cshtml.cs
@@ -39,7 +39,7 @@ namespace MegaDesk_RazorPages
             _context.Desk.Add(Desk);
             await _context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("./View");
         }
     }
 }
