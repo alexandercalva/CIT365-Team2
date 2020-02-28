d465cd641525aca66ab905cd78919c8c0f20dad2
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2020-02-28 12:42:48 -0500

    Math operations

--- Pages/Desks/Edit.cshtml.cs
+++ Pages/Desks/Edit.cshtml.cs
@@ -48,9 +48,63 @@ namespace MegaDesk_RazorPages
                 return Page();
             }
 
-            var widthForm = int.Parse(Request.Form["width"]);
-            Desk.Width = widthForm;
-            Desk.Price = widthForm;
+            //***************************************************************************************
+
+            //ADD code to calculate the final price
+            var materials = 0; ;
+            var orderRush = 0;
+            var basePrice = 200;
+            // Calculate
+            var widthNumber = int.Parse(Request.Form["width"]);
+            Desk.Width = widthNumber;
+            var depthNumber = int.Parse(Request.Form["depth"]);
+            Desk.Depth = depthNumber;
+            var area = widthNumber * depthNumber; // $1 per in2
+            var drawersNumber = int.Parse(Request.Form["drawers"]);
+            Desk.Drawers = drawersNumber;
+            var drawers = drawersNumber * 50;
+            var material = Request.Form["material"];
+            Desk.Material = material;
+            var order = int.Parse(Request.Form["order"]);
+            Desk.Order = order;
+            if (material == "Laminate")
+                materials = 100;
+            else if (material == "Oak")
+                materials = 200;
+            else if (material == "Pine")
+                materials = 50;
+            else if (material == "Rosewood")
+                materials = 300;
+            else if (material == "Veneer")
+                materials = 125;
+
+            if (order == 3 && area < 1000)
+                orderRush = 60;
+
+            else if (order == 5 && area < 1000)
+                orderRush = 40;
+            else if (order == 7 && area < 1000)
+                orderRush = 30;
+
+            if (order == 3 && (area >= 1000 && area <= 2000))
+                orderRush = 70;
+            else if (order == 5 && (area >= 1000 && area <= 2000))
+                orderRush = 50;
+            else if (order == 7 && (area >= 1000 && area <= 2000))
+                orderRush = 35;
+
+            if (order == 3 && area > 2000)
+                orderRush = 80;
+            else if (order == 5 && area > 2000)
+                orderRush = 60;
+            else if (order == 7 && area > 2000)
+                orderRush = 40;
+
+            Desk.Price = basePrice + area + drawers + materials + orderRush;
+
+            //**************************************************************************************
+
+
             _context.Attach(Desk).State = EntityState.Modified;
 
             try
